459955287f09bca2693c2084f243e822faedfcb2
Dinh-Thao <nguyendinhthao345@gmail.com>
2023-05-03 15:07:50 +0700

    c

--- store.php
+++ store.php
@@ -1,58 +1,80 @@
 
-<?php require_once("db.php"); ?>
+<?php 
+    require_once("db.php"); 
+    $sql = "SELECT Title, Author, Price FROM books";
+    $result = $conn->query($sql);
 
 
+    $limit = 10;
+    $page = 0;
+    $output = '';
 
-<form name="search" method="post">
-    <input type="text" id="search" name="search"/>
-    <button type="submit" value="search"
-</form>
-
-<div class="container-fluid mt-5">
-    <?php 
-        $number= 1;
-        $numbers = array(1,2,3);
-        $result_per_page = 10;
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if(empty($_POST["search"])) {
-            $sql = "SELECT title, author, price, image FROM books";
-        }
-        else {
-            $key = $_POST["search"];
-            $sql = "SELECT title, author, price, image FROM books WHERE title = '$key' OR Author = '$key'";
-        }
-
-        $result = $conn->query($sql);
-        $number_of_result = mysqli_num_rows($result);
-        $number_of_page = ceil ($number_of_result / $result_per_page);  
-        
-        $sql = $sql.' LIMIT ' . $result_per_page;
-        // echo $sql;
-        $result = $conn->query($sql);
-        while ($row = mysqli_fetch_array($result)) {  
-            echo $row['title'] . ' -By.  ' . $row['author'] . ' -Price: ' . $row['price'] . '</br>';  
-        }  
+    if(isset($_POSTR["page"])) {
+        $page = $_POST["page"];
+    } else {
+        $page = 1;
     }
 
-    ?>
-    <nav aria-label="Page navigation example">
-    <ul class="pagination mt-5">
-        <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-        </a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-        </a>
-        </li>
-    </ul>
-    </nav>
-</div>
+    $start_from =($page - 1) * $limit;
+    $query = mysqli_query($conn, "SELECT Title, Author, Price FROM books ORDER BY Title ASC LIMIT $start_from, $limit" );
+    $output .= '
+        <div class="container-fluid mt-5">
+            <table class="table table-hover">
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Price</th>
+                </tr>
+    ';
+    if(mysqli_num_rows($query) > 0) {
+        while($row = mysqli_fetch_array($query)) {
+            $output .= '
+                <tr>
+                    <td>'.ucfirst($row["Title"]).'</td>
+                    <td>'.ucfirst($row["Author"]).'</td>
+                    <td>'.ucfirst($row["Price"]).'</td>
+                </tr>
+            ';
+        }
+    } else {
+        $output.='
+            <tr>
+                <td> Data not Found. </td>
+            </tr>
+        ';
+    }
+
+    $output .= '
+        </table>
+        </div>
+    ';
+
+    //pagination
+    $query = mysqli_query($conn, "SELECT Title, Author, Price FROM books");
+    $total_records = mysqli_num_rows($query);
+    $total_pages = ceil($total_records/$limit);
+    $output .= '<ul class="pagination">';
+
+    if($page > 1) {
+        $previous = $page - 1;
+        $output .= '<li class="page-item" id="1"><span class="page-link">First Page</span></li>';
+        $output .= '<li class="page-item" id="'.$previous.'"><span class="page-link"><i class="fa fa-arrow-left"></i></span></li>';
+    }
+
+    for($i=1; $i<=$total_pages; $i++) {
+        $active_class = "";
+        if($i == $page) {
+            $active_class ="active";
+        }
+        $output .= '<li class="page-item"'.$active_class.'" id="'.$i.'"><span class="page-link">'.$i.'</span></li>';
+    }
+
+    if($page < $total_pages) {
+        $page++;
+        $output .= '<li class="page-item" id="'.$page.'"><span class="page-link"><i class ="fa fa-arrow-right"></i></span></li>';
+        $output .= '<li class="page-item" id="'.$total_pages.'"><span class="page-link">Last Page</span></li>';
+    }
+    $output .= '</ul>';
+    echo $output;
+?>
+
